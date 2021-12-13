a0feea25a5d66895b34c4d593427236612286ba8
Sandeep Reddy <adithyaankinapalli6@gmail.com>
2021-12-13 13:34:03 +0530

    Time: 12 ms (77.78%), Space: 10.8 MB (43.98%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,6 +1,11 @@
+#include <map>
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        //O(n^2) Solution
+        /*
         vector<int> vect;
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
@@ -12,5 +17,18 @@ public:
             }
         }
         return vect;
+        */
+        
+        unordered_map<int, int> val;
+        vector<int> vect;
+        
+        for(int i=0;i<nums.size();i++){
+            int numToFind = target - nums[i];
+            if(val.find(numToFind)!=val.end()){
+                return {val[numToFind],i};
+            }
+            val[nums[i]]=i;
+        }
+        return {};
     }
 };
