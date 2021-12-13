2bc3dc21493ca3d21ca24f2ba1c7c0a512e5913d
Sandeep Reddy <adithyaankinapalli6@gmail.com>
2021-12-13 13:40:52 +0530

    Time: 36 ms (52.58%), Space: 20.1 MB (66.83%) - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -4,35 +4,27 @@ class Solution {
 public:
     
     bool containsDuplicate(vector<int>& nums) {
-        
-        if(nums.empty() || nums.size()==1){
-            return false;
-        }
-            
-        sort(nums.begin(), nums.end());
-        
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i+1]==nums[i]){
-                return true;
-            }
-        }
-        
-        return false;
-        
+        //O(n^2) Solution
         /*
-        BRUTE FORCE SOLUTION O(N^2)
-        
-        if(nums.empty()) return false;
-
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size() && j!=i;j++){
-                if(nums[j]==nums[i]){
-                    return true;
+        bool val=false;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    val = true;
+                    break;
                 }
             }
         }
-        
-        return false;
+        return val;
         */
+        unordered_map<int,int> hash;
+        
+        for(int i=0;i<nums.size();i++){
+            if(hash.find(nums[i])!=hash.end()){
+                return true;
+            }
+            hash[nums[i]]=i;
+        }
+        return false;
     }
 };
